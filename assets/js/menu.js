b3fb583fed8e5c0fc08cc9dad32ea66cda289376
MariQerdika <40671485+MariQerdika@users.noreply.github.com>
2021-12-09 23:22:36 +0400

    add animated left menu

---
+++ assets/js/menu.js
@@ -0,0 +1,9 @@
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const menu = document.querySelector('.menu__container');
+
+open.addEventListener('click', () => menu.classList.add('show-nav'));
+
+close.addEventListener('click', () => menu.classList.remove('show-nav'));
+
+
